41b33cc9880214af65e0daa80777fc6df84b9735
Aaron Bray <aaron.bray@kitware.com>
2017-08-16 14:06:06 -0400

    License boiler plate update

--- sdk/howto/cpp/EngineHowTo.h
+++ sdk/howto/cpp/EngineHowTo.h
@@ -1,14 +1,5 @@
-/**************************************************************************************
-Copyright 2015 Applied Research Associates, Inc.
-Licensed under the Apache License, Version 2.0 (the "License"); you may not use
-this file except in compliance with the License. You may obtain a copy of the License
-at:
-http://www.apache.org/licenses/LICENSE-2.0
-Unless required by applicable law or agreed to in writing, software distributed under
-the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
-CONDITIONS OF ANY KIND, either express or implied. See the License for the
-specific language governing permissions and limitations under the License.
-**************************************************************************************/
+/* Distributed under the Apache License, Version 2.0.
+   See accompanying NOTICE file for details.*/
 
 // Note that this project is set with the following Additional Include Paths: ../include;../include/cdm;../include/cdm/bind
 // This will build an executable that is intended to execute a how-to method
