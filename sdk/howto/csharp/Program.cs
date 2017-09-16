8599f8c7ffc7d962bf05075e8f3f25e193a5e1e4
Aaron Bray <aaron.bray@kitware.com>
2017-09-15 20:53:52 -0400

    Improved threaded C# example

--- sdk/howto/csharp/Program.cs
+++ sdk/howto/csharp/Program.cs
@@ -1,28 +1,49 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Threading;
 
 namespace HowToCSharp
 {
-
-
-    class Program
+  class Program
   {
+    static void Main(string[] args)
+    {
+      RealTimePulse pulse = new RealTimePulse("RealTimePulse.log", "./states/StandardMale@0s.pba");
 
-        private static System.Threading.Timer Timer;
-        static public int smallesttimestep = 200;
-        public double e = 0.01;
-        static void Main(string[] args)
-         {
-            test T = new test();
+      // Create the thread object, passing in the Alpha.Beta method
+      // via a ThreadStart delegate. This does not start the thread.
+      Thread oThread = new Thread(new ThreadStart(pulse.run));
 
-            while (true)
-            {
+      // Start the thread
+      oThread.Start();
 
-            }
-         }
+      // Spin for a while waiting for the started thread to become alive:
+      while (!oThread.IsAlive);
 
+      // Put the Main thread to sleep for a while to allow pulse to do some work:
+      int seconds = 0;
+      while (seconds < 60)
+      {
+        Thread.Sleep(1000);
+        Console.WriteLine("Lung Volume (mL) : " + pulse.GetLungVolume_mL() + "\n" +
+                          "Respiratory Rate (1/min) : " + pulse.GetRespiratoryRate_per_min() + "\n" +
+                          "Muscle Pressure (cmH2O) : " + pulse.GetMusclePressure_cmH2O() + "\n" +
+                          "Maximum Muscle Pressure (cmH2O) : " + pulse.GetMaxMusclePressure_cmH2O() + "\n" +
+                          "Minimum Muscle Pressure (cmH2O) : " + pulse.GetMinMusclePressure_cmH2O() + "\n" +
+                          "Pulmonary Compliance (mL/cmH2O) : " + pulse.GetPulmonaryCompliance_mL_per_cmH2O() + "\n" +
+                          "Pulmonary Resistance (cmH2O s/L) : " + pulse.GetPulmonaryResistance_cmH2O_s_Per_L() + "\n");
+        seconds++;
+      }
+
+      // Request that oThread be stopped
+      oThread.Abort();
+
+      // Wait until oThread finishes. Join also has overloads
+      // that take a millisecond interval or a TimeSpan object.
+      oThread.Join();
+
+      Console.WriteLine();
+      Console.WriteLine("Pulse has finished");
     }
+
+  }
 }
---
+++ sdk/howto/csharp/RealTimePulse.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Threading;
+using System.Diagnostics;
+
+// Look at James' email on strings
+// fix the CLR install (CLR is not part of all build, so there is nothing to install, so install fails)
+// How do we get events into C#
+// Make properties and other cdm objects
+// units should be an enum on C# side of things
+
+namespace HowToCSharp
+{
+  public class RealTimePulse
+  {
+    protected bool _alive = true;
+    protected bool _paused = false;
+    protected PulseEngineRef _pulse;
+    private Object _lock = new Object();
+
+    protected double _lung_volume_mL;
+    protected double _respiratory_rate_per_min;
+    protected double _muscle_pressure_cmH2O;
+    protected double _pulmonary_compliance_mL_per_cmH2O;
+    protected double _pulmonary_resistance_cmH2O_s_Per_L;
+    protected double _max_muscle_pressure_cmH2O;
+    protected double _working_max_muscle_pressure_cmH2O;
+    protected double _min_muscle_pressure_cmH2O;
+    protected double _working_min_muscle_pressure_cmH2O;
+
+    public RealTimePulse() { }
+    public RealTimePulse(string log_file, string state_file)
+    {
+      _pulse = new PulseEngineRef(log_file);
+      _pulse.LoadStateFile(state_file);
+    }
+
+    public double GetLungVolume_mL() { lock(_lock) { return _lung_volume_mL; } }
+    public double GetRespiratoryRate_per_min() { lock (_lock) { return _respiratory_rate_per_min; } }
+    public double GetMusclePressure_cmH2O() { lock (_lock) { return _muscle_pressure_cmH2O; } }
+    public double GetMaxMusclePressure_cmH2O() { lock (_lock) { return _max_muscle_pressure_cmH2O; } }
+    public double GetMinMusclePressure_cmH2O() { lock (_lock) { return _min_muscle_pressure_cmH2O; } }
+    public double GetPulmonaryCompliance_mL_per_cmH2O() { lock (_lock) { return _pulmonary_compliance_mL_per_cmH2O; } }
+    public double GetPulmonaryResistance_cmH2O_s_Per_L() { lock (_lock) { return _pulmonary_resistance_cmH2O_s_Per_L; } }
+
+    public void StartBreath()
+    {
+      _max_muscle_pressure_cmH2O = _working_max_muscle_pressure_cmH2O;
+      _working_max_muscle_pressure_cmH2O = 0;
+      _min_muscle_pressure_cmH2O = _working_min_muscle_pressure_cmH2O;
+      _working_min_muscle_pressure_cmH2O = 1000000;
+    }
+
+    public void run()
+    {
+      int duration_ms;
+      Console.WriteLine("Pulse is running in its own thread.");
+      Stopwatch stopWatch = new Stopwatch();
+      while (_alive)
+      {
+        stopWatch.Reset();
+        stopWatch.Start();
+        if (!_paused)
+        {
+          _pulse.AdvanceModelTime(1);// Advance a second
+          lock(_lock)
+          {
+            _lung_volume_mL = _pulse.GetLungVolume();
+            _respiratory_rate_per_min = _pulse.GetRespiratoryRate();
+            _muscle_pressure_cmH2O = _pulse.GetMusclePressure();
+            _pulmonary_compliance_mL_per_cmH2O = _pulse.GetPulmonaryCompliance();
+            _pulmonary_resistance_cmH2O_s_Per_L = _pulse.GetPulmonaryResistance();
+            if (_muscle_pressure_cmH2O > _working_max_muscle_pressure_cmH2O)
+              _working_max_muscle_pressure_cmH2O = _muscle_pressure_cmH2O;
+            if (_muscle_pressure_cmH2O < _working_min_muscle_pressure_cmH2O)
+              _working_min_muscle_pressure_cmH2O = _muscle_pressure_cmH2O;
+          }
+        }
+        stopWatch.Stop();
+        // See how long it took us to run a second, and sleep
+        duration_ms = (int)stopWatch.Elapsed.TotalMilliseconds;
+        if(duration_ms > 1000)
+          Console.WriteLine("Pulse is running slower than real time!!! : "+ duration_ms);
+        else
+          Thread.Sleep(1000 - duration_ms);
+      }
+    }
+  }
+}
